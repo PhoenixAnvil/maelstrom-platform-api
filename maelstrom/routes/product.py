589bee505f3f0a3849a3c8e4d297b80a74fcfad3
Jason Alan Smith <jas@phoenixanvilabs.dev>
2025-04-22 21:44:57 -0500

    feat: add product post route

--- maelstrom/routes/product.py
+++ maelstrom/routes/product.py
@@ -4,6 +4,7 @@ from sqlalchemy.future import select
 
 from maelstrom.db import get_db
 from maelstrom.models.product import Product
+from maelstrom.schemas.product import ProductCreate
 from maelstrom.schemas.product import ProductOut
 
 router = APIRouter(prefix="/products", tags=["Products"])
@@ -13,3 +14,12 @@ router = APIRouter(prefix="/products", tags=["Products"])
 async def list_products(db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Product))
     return result.scalars().all()
+
+
+@router.post("/", response_model=ProductOut)
+async def create_product(product: ProductCreate, db: AsyncSession = Depends(get_db)):
+    new_product = Product(**product.dict())
+    db.add(new_product)
+    await db.commit()
+    await db.refresh(new_product)
+    return new_product
